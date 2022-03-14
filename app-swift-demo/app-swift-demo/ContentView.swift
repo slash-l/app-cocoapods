d92b1b7ed32be902761f3f2137d4b98f1559a1a8
slash-l <slash_mumu@163.com>
2022-03-15 00:37:04 +0800

    code init

---
+++ app-swift-demo/app-swift-demo/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  demo-app
+//
+//  Created by Jing Yi Liu on 2022/3/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                VStack {
+                    Text("Hello JFrog")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.green)
+                        .padding()
+                        
+                    ColorPicker("This is a simple swift app. Let's do it!", selection: /*@START_MENU_TOKEN@*/.constant(.red)/*@END_MENU_TOKEN@*/)
+                        .padding(.leading, 16.0)
+                }
+                HStack {
+                    DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { Text("Date") })
+                    
+                    Button("确认") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    
+                }
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app-swift-demo/app-swift-demo/app_swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_swift_demoApp.swift
+//  app-swift-demo
+//
+//  Created by Jing Yi Liu on 2022/3/14.
+//
+
+import SwiftUI
+
+@main
+struct app_swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
