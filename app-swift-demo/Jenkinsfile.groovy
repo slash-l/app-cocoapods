878fd058070c7cf1e8d1f4bff509cc8048e1459b
slash-l <slash_mumu@163.com>
2022-03-15 10:16:17 +0800

    add Jenkinsfile.groovy

---
+++ app-swift-demo/Jenkinsfile.groovy
@@ -0,0 +1,61 @@
+def server
+def SERVER_NAME = "saas-server"
+def buildInfo
+def promotionConfig
+
+node{
+    stage("Artifactory config"){
+        server = Artifactory.server SERVER_NAME
+
+    }
+    
+    stage("SCM Check out"){
+        git branch: "main", url: 'https://github.com/slash-l/app-cocoapods.git'
+    }
+    
+    stage("Pod install"){
+        def pod_install_result = sh returnStatus: true, script: "cd app-swift-demo"
+        //pod_install_result = pod_install_result.trim()
+        echo "pod install result:" + pod_install_result
+    }
+    
+    stage("Pod upload"){
+        def pwd = sh returnStatus: true, script: "pwd"
+        echo "pwd:" + pwd
+        
+        // def pod_spec_result = sh returnStatus: true, script: "cd app-swift-demo && /usr/local/bin/pod spec create app-swift-demo && cd .. && tar -czvf app-swift-demo.tar.gz app-swift-demo"
+        // pod_spec_result = pod_spec_result.trim()
+        // echo "pod spec result:" + pod_spec_result
+        
+        // upload to Artifactory
+        
+    }
+    
+    stage("Set Props"){
+        
+    }
+    
+    stage("Publish Build Info"){
+        
+    }
+    
+    stage("Promotion"){
+        // promotionConfig = [
+        //     //Mandatory parameters
+        //     'buildName'          : buildInfo.name,
+        //     'buildNumber'        : buildInfo.number,
+        //     'targetRepo'         : "slash-cocoapods-test-local",
+
+        //     //Optional parameters
+        //     'comment'            : 'this is the promotion comment',
+        //     'sourceRepo'         : "slash-cocoapods-dev-local",
+        //     'status'             : 'Released',
+        //     'includeDependencies': true,
+        //     'failFast'           : true,
+        //     'copy'               : true
+        // ]
+
+        // // Promote build
+        // server.promote promotionConfig
+    }
+}
